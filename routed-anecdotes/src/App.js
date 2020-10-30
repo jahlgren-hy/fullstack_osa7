fb8b43777fe233ab567f31402c9e0721928a5cc9
Jukka Ahlgren <jukka.ahlgren@helsinki.fi>
2020-10-30 03:13:58 +0200

    7.4: anekdoottisovellus ja hookit step1
    Yksinkertaista sovelluksen uuden anekdootin luomiseen käytettävän lomakkeen käyttöä äsken määritellyn useField custom-hookin avulla.
    
    	- modified:   routed-anecdotes/src/App.js
    	- new file:   routed-anecdotes/src/hooks/index.js

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -4,7 +4,9 @@ import {
   Switch, Route, Link,
   useHistory,
   useParams
-} from "react-router-dom"
+} from 'react-router-dom'
+
+import { useField } from './hooks'
 
 const Anecdote = ({ anecdotes }) => {
   const id = useParams().id
@@ -65,17 +67,16 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const history = useHistory()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push('/')
@@ -87,15 +88,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -165,7 +166,7 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = (Math.random() * 10000).toFixed(0)
     setAnecdotes(anecdotes.concat(anecdote))
-    setNotification(`a new anecdote '${anecdote.content}' creted`)
+    setNotification(`a new anecdote '${anecdote.content}' created`)
     setTimeout(() => {
       setNotification('')
     }, 5000)
